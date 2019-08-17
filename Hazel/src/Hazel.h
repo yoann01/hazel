dd40dcd6be341c00f8520ffe2eb64840957c303a
yoann01 <ygranier@gmail.com>
2019-08-17 16:04:41 +0200

    Setup basic application

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include<stdio.h>
+
+//For use by hazel application
+#include "hazel/Application.h"
+
+//--Entry point
+#include"hazel/EntryPoint.h"
---
+++ Hazel/src/hazel/EntryPoint.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifdef HZ_PLATFORM_WINDOWS
+
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+
+int main(int argc, char** argv)
+{
+	printf("Loading engine\n");
+	auto app = Hazel::CreateApplication(); 
+
+	app->Run();
+
+	delete app;
+}
+
+
+#endif // HZ_PLATFORM_WINDOWS
